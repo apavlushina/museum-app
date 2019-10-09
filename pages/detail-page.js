b5acc59b611cc537af178244abf4b0db0dcadc4b
apavlushina <ssemina7@gmail.com>
2019-10-09 14:24:18 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,18 @@
 function submitComment () {
-    console.log("Hello");
+    let inputField = document.getElementById('name');
+    let name = inputField.value;
+    let textArea = document.getElementById('msg');
+    let msg = textArea.value;
+    let comment = document.createElement('section');
+    let h3 = document.createElement('h3');
+    let paragraph = document.createElement('p');
+
+    h3.textContent = `${name} said:`;
+    paragraph.textContent = `${msg}`;
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(paragraph);
+
+
+    console.log(comment);
 }
