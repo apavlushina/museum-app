f629e1ed58ef7820673a4c2a8b59cb4f7d4b7636
apavlushina <ssemina7@gmail.com>
2019-10-09 16:02:37 +0200

    Set Up Form Validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,22 +1,66 @@
-function submitComment () {
-    let inputField = document.getElementById('name');
-    let name = inputField.value;
-    let textArea = document.getElementById('msg');
-    let msg = textArea.value;
-    let comment = document.createElement('section');
-    let h3 = document.createElement('h3');
-    let paragraph = document.createElement('p');
+'use strict'
 
-    h3.textContent = `${name} said:`;
-    paragraph.textContent = `"${msg}"`;
-    comment.classList.add('comment');
-    comment.appendChild(h3);
-    comment.appendChild(paragraph);
 
-    commentSection = document.getElementById('comments');
-    commentSection.appendChild(comment)
-    
-    
-    inputField.value = null;
-    textArea.value = null;
+function passAllValidation (msg, name) {
+    if (msg || name) {
+        if (msg.length < 280) {
+            return true
+        } else {
+            alert('You message is too long!');
+        }
+    } else {
+        return alert('You forgot to fill in your name or message!');
+    }
 }
+
+
+function submitComment() {
+const inputField = document.getElementById('name');
+const name = inputField.value;
+const textArea = document.getElementById('msg');
+const msg = textArea.value;
+const comment = document.createElement('section');
+const h3 = document.createElement('h3');
+const paragraph = document.createElement('p');
+
+    if (passAllValidation (msg, name)) {
+            h3.textContent = `${name} said:`;
+            paragraph.textContent = `"${msg}"`;
+            comment.classList.add('comment');
+            comment.appendChild(h3);
+            comment.appendChild(paragraph);
+
+            let commentSection = document.getElementById('comments');
+            commentSection.appendChild(comment);
+
+            inputField.value = null;
+            textArea.value = null;
+    } 
+}
+
+
+
+
+
+
+//     if (msg || name) {
+//         if (msg.length < 280) {
+//             h3.textContent = `${name} said:`;
+//             paragraph.textContent = `"${msg}"`;
+//             comment.classList.add('comment');
+//             comment.appendChild(h3);
+//             comment.appendChild(paragraph);
+
+//             commentSection = document.getElementById('comments');
+//             commentSection.appendChild(comment)
+
+//             inputField.value = null;
+//             textArea.value = null;
+//         } else {
+//             alert('You message is too long!');
+//         }
+
+//     } else {
+//         return alert('You forgot to fill in your name or message!');
+//     }
+// }
